d2a2dc85816cbf9f7c191e48ebc8d0862a6f5b89
Kilian <kiliangonzalez@ceroca.cat>
2024-11-18 16:20:02 +0100

    Código vidas cuando chocas

--- Assets/codigos/crash.cs
+++ Assets/codigos/crash.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Crash : MonoBehaviour
 {
-    public int vidas = 3;
+    public int vidas;
+    [SerializeField] private TMPro.TextMeshProUGUI uiVidasJugador;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        vidas = 3;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("enemy"))
         {
             vidas--;
-
+             
+            
             if (vidas <= 0)
             {
                 Destroy(gameObject);
@@ -20,7 +29,7 @@ public class Crash : MonoBehaviour
             }
             else
             {
-                Debug.Log("Vidas restantes: " + vidas);
+                uiVidasJugador.text = vidas.ToString() + " x";
             }
         }
     }
